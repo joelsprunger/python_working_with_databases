d4c7193ab193788eb967cd8fac2a06c567e6d41f
joelsprunger <43421397+joelsprunger@users.noreply.github.com>
2019-07-25 15:07:19 -0700

    Very basic database test script students.py

--- students.py
+++ students.py
@@ -1,4 +1,5 @@
-from peewee import*
+from peewee import *
+from peewee import SqliteDatabase
 
 db = SqliteDatabase('students.db')
 
@@ -10,6 +11,37 @@ class Student(Model):
     class Meta:
         database = db
 
+
+students = [
+    {'username': 'joelsprunger',
+     'points': 2793},
+    {'username': 'rafaelcolon',
+     'points': 2792},
+    {'username': 'kjirkwatkis',
+     'points': 1793},
+    {'username': 'danasprunger',
+     'points': 2793000}
+]
+
+
+def add_students():
+    for student in students:
+        try:
+            Student.create(username=student['username'],
+                           points=student['points'])
+        except IntegrityError:
+            student_record = Student.get(username=student['username'])
+            student_record.points = student['points']
+            student_record.save()
+
+
+def top_student():
+    student = Student.select().order_by(Student.points.desc()).get()  # sort in descending order get 1st record
+    return student
+
+
 if __name__ == '__main__':
     db.connect()
     db.create_tables([Student], safe=True)
+    add_students()
+    print("Our top student right now is {0.username}.".format(top_student()))
