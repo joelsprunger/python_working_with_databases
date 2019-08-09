3a8af44da186f642e2ebc98448a037a0d21f3666
joelsprunger <43421397+joelsprunger@users.noreply.github.com>
2019-08-09 09:34:19 -0700

    Diary program is complete

--- diary.py
+++ diary.py
@@ -2,6 +2,7 @@
 
 from collections import OrderedDict
 import datetime
+import os
 import sys
 
 from peewee import *
@@ -23,23 +24,28 @@ def initialize():
     db.create_tables([Entry], safe=True)
 
 
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 def menu_loop():
     """Show the menu"""
     choice = None
 
     while choice != 'q':
+        clear()
         print("Enter 'q' to quit.")
         for key, value in menu.items():
             print('{}) {}'.format(key, value.__doc__))
         choice = input('Action: ').lower().strip()
 
         if choice in menu:
+            clear()
             menu[choice]()
 
 
 def add_entry():
     """Add an entry."""
-    print("Enter your entry. Press ctrl+d when finished.")
+    print("Enter your entry. Press ctrl+z when finished.")
     data = sys.stdin.read().strip()
 
     if data:
@@ -48,17 +54,44 @@ def add_entry():
             print("Entry saved successfully")
 
 
-def view_entries():
+def view_entries(search_query=None):
     """view previous entries"""
+    entries = Entry.select().order_by(Entry.timestamp.desc())
+    if search_query:
+        entries = entries.where(Entry.content.contains(search_query))
+
+    for entry in entries:
+        timestamp = entry.timestamp.strftime('%A AB %d, %Y %I:%M%p')
+        clear()
+        print(timestamp)
+        print('='*len(timestamp))
+        print(entry.content)
+        print('\n\n'+'=' * len(timestamp))
+        print('n) for next entry')
+        print('d) for delete entry')
+        print('q) return to main menu')
+
+        next_action = input('Action [Ndq]: ').lower().strip()
+        if next_action == 'q':
+            break
+        if next_action == 'd':
+            delete_entry(entry)
+
+def search_entries():
+    """Search entries for a string."""
+    view_entries(input('Search query: '))
 
 
 def delete_entry(entry):
     """Delete and entry"""
-
+    if input("Are you sure? [yN] ").lower() == 'y':
+        entry.delete_instance()
+        print("Entry deleted!")
 
 menu = OrderedDict([
     ('a', add_entry),
     ('v', view_entries),
+    ('s', search_entries),
 ])
 
 
